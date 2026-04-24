ff13667c51db0c33e8d1e4e2e7abb8570c584e0f
Ksenia <kseniplatonenko@gmail.com>
2026-04-24 11:12:16 +0700

    вывод пользователей

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -2,7 +2,7 @@
 URL configuration for blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
 from login import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('add_user/', views.add_user),
     path('users/', views.users),
+    path('', views.index),
+    path('login/', views.login),
+    path('logout/', views.logout_view),
+    
 ]
--- blog/login/views.py
+++ blog/login/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from .models import User
 from .forms import UserForm
 
+def users(request):
+    if not request.session.get('user_id'):
+        return redirect('/login/')
+    users = User.objects.all()
+    return render(request, 'users.html', {'users': users})
 
 def add_user(request):
     if request.method == "POST":
         user = UserForm(request.POST)
         if user.is_valid():
             user.save()
-        return redirect('/')
+        return redirect('/users/')
     else:
         form = UserForm()
         return render(request, "add_user.html", {'form': form})
+
+def index(request):
+    if request.session.get('user_id'):
+        l = request.session.get('login')
+        return render(request, 'index.html', {'login': l})
+    else:
+        return redirect('/login/')
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        login = request.POST.get('login')
+        password = request.POST.get('pas')
+    try:
+        user = User.objects.get(login=login)
+    except User.DoesNotExist:
+        return redirect('/login/')
+    if password != user.password:
+        return redirect('/login/')
+    request.session['user_id'] = user.id
+    request.session['login'] = user.login
+    return redirect('/')
+
+def logout_view(request):
+    request.session.flush()
+    return redirect('/login/')
